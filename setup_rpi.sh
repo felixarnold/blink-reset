043d4379a15162d22333f5e65b09b307f28ee780
Felix Arnold <felix.julian.arnold@gmail.com>
2023-06-16 11:56:49 +0200

    new script to setup raspi for operation

---
+++ setup_rpi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Load the I2C kernel modules
+modprobe i2c_dev
+modprobe i2c_bcm2708
+
+# Enable I2C on the Raspberry Pi
+echo "dtparam=i2c1=on" >> /boot/config.txt
+echo "dtparam=i2c_arm=on" >> /boot/config.txt
+
+# Add the I2C devices to the modules file
+echo "i2c-dev" >> /etc/modules
+echo "i2c-bcm2708" >> /etc/modules
+
+# Restart the Raspberry Pi to apply the changes
+reboot
