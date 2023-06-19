76c4548d1f8a2daf88a27c1aafc9fc8e2ab421e7
Felix Arnold <felix.julian.arnold@gmail.com>
2023-06-19 13:27:46 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -15,18 +15,18 @@ try_echo () {
     fi
 }
 
-echo "Setup environment"
+echo "# Setup environment"
 sudo apt update && sudo apt upgrade -y
 # sudo apt install python3-venv
 # python3 -m venv venv
 # source venv/bin/activate
 sudo pip install -r requirements.txt
 
-echo "Load the I2C kernel modules"
+echo "# Load the I2C kernel modules"
 modprobe i2c_dev
 modprobe i2c_bcm2708
 
-echo "Enable I2C on the Raspberry Pi"
+echo "# Enable I2C on the Raspberry Pi"
 setting1="dtparam=i2c1=on"
 setting2="dtparam=i2c_arm=on"
 path="/boot/config.txt"
@@ -35,22 +35,21 @@ try_echo "dtparam=i2c1=on" $path
 try_echo "dtparam=i2c_arm=on" $path
 # echo "dtparam=i2c_arm=on" >> /boot/config.txt
 
-echo "Add the I2C devices to the modules file"
+echo "# Add the I2C devices to the modules file"
 path="/etc/modules"
 try_echo "i2c-dev" $path
 # echo "i2c-dev" >> /etc/modules
 try_echo "i2c_bcm2708" $path
 # echo "i2c-bcm2708" >> /etc/modules
 
-echo "Setup udev config"
+echo "# Setup udev config"
 text="SUBSYSTEM==\"usb\", ACTION==\"add\", RUN+=\"$HOME/blink-reset/run-blink-reset.sh\""
 path="/etc/udev/rules.d/99-reset-device.rules"
 try_echo "$text" $path
 # echo 'SUBSYSTEM=="usb", ACTION=="add", RUN+="$HOME/blink-reset/run-blink-reset.sh"' >> /etc/udev/rules.d/99-reset-device.rules
 
 # Restart the Raspberry Pi to apply the changes
-read -p "Reboot now? (y/n) " -n 1 -r
-echo
+read -p "Reboot now? (y/n) " -n 1 -r; echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     reboot
